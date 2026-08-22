78948817833c1743c116183dafb3c263ab50ee1a
gilsoncabacazzo <gilsoncaballero@gmail.com>
2026-08-22 09:14:10 -0300

    se corrige el path en la lambda para consultar los examenes por turno

--- index.mjs
+++ index.mjs
@@ -49,7 +49,7 @@ export const handler = async (event) => {
         switch (event.resource) {
           case "/examenes/{id}":
             return await obtenerExamenPorId(pathParameters.id, consultorio_id);
-          case "/examenes/{id}/receta":
+          case "/turnos/{id}/examenes":
             return await obtenerExamenesPorTurno(pathParameters.id, consultorio_id);
           default:
             return response(404, { error: "Ruta no encontrada." });
